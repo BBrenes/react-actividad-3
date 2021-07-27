6403f4168a6486eb8a655f27793befc02a8ee395
Brian Brenes <bbrenes14@gmail.com>
2021-07-27 18:09:29 -0500

    Clear comment function added

--- src/components/WriteComment.js
+++ src/components/WriteComment.js
@@ -33,6 +33,7 @@ export default function WriteComment({id}) {
           comment !== ""    
         ){
             dispatch(addComment({comment:comment, id:id}))
+            setComment('')
         }
       }
 
